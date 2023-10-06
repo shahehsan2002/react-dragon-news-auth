623ef8422e8de8cf1c0dbe9ff714c232136592bc
shahehsan2002 <shahehsan2002@.com>
2023-10-06 21:27:33 +0600

    breaking News area created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import Header from "../shared/Header/Header";
 import LeftSideNav from "../shared/LefttSideNav/LeftSideNav";
 import Navbar from "../shared/Navbar/Navbar";
 import RightSideNav from "../shared/RightSideNav/RightSideNav";
+import BreakingNews from "./BreakingNews";
 
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <BreakingNews></BreakingNews>
             <Navbar></Navbar>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
                 <div>
