b9f895026028c9e87e7bad5a8efb5fd42335496e
shahehsan2002 <shahehsan2002@.com>
2023-10-06 23:14:53 +0600

    Right side Navigatiin and info with icons

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,9 +1,45 @@
+import { Link, NavLink } from "react-router-dom";
+import UserDefaultPicture from '../../../assets/user.png';
 
 
 const Navbar = () => {
+
+    const NavLinks = <>
+        <li><NavLink to='/'>Home</NavLink></li>
+        <li><NavLink to='/about'>About</NavLink></li>
+        <li><NavLink to='/career'>Career</NavLink></li>
+
+    </>
     return (
-        <div>
-            <h2 className="text-2xl">Navbar</h2>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                       <img src={UserDefaultPicture} alt="" />
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+
+                        {NavLinks}
+
+                    </ul>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {NavLinks}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                    <div className="w-10 rounded-full">
+                        <img src={UserDefaultPicture} />
+                    </div>
+                </label>
+                <Link>
+                    <button className="btn">Login</button>
+                </Link>
+
+            </div>
         </div>
     );
 };
