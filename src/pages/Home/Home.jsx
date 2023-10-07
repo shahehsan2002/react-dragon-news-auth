70b03ac3c8936f1df44cb6b1993a98ca840cc486
shahehsan2002 <shahehsan2002@.com>
2023-10-07 19:11:46 +0600

    load news and create News detail page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
+import { useLoaderData } from "react-router-dom";
 import Header from "../shared/Header/Header";
 import LeftSideNav from "../shared/LefttSideNav/LeftSideNav";
 import Navbar from "../shared/Navbar/Navbar";
 import RightSideNav from "../shared/RightSideNav/RightSideNav";
 import BreakingNews from "./BreakingNews";
+import NewsCard from "./NewsCard";
+
 
 
 const Home = () => {
+    const news = useLoaderData();
+    console.log(news);
     return (
         <div>
             <Header></Header>
@@ -15,8 +20,14 @@ const Home = () => {
                 <div>
                     <LeftSideNav></LeftSideNav>
                 </div>
+                {/* news container */}
                 <div  className="md:col-span-2">
-                    <h2 className="text-3xl">News Coming Soon</h2>
+                    {
+                        news.map(aNews => <NewsCard
+                        key={aNews._id}
+                        news={aNews}
+                        ></NewsCard>)
+                    }
                 </div>
                 <div>
                     <RightSideNav></RightSideNav>
---
+++ src/pages/News/News.jsx
@@ -0,0 +1,9 @@
+const News = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">News Details</h2>
+        </div>
+    );
+};
+
+export default News;
